52bb17193359555562a3c7ca77f5d318ec5c735f
Ivana Andersson <ivana.p.kostova@gmail.com>
2021-02-12 12:43:02 +0200

    add webpack config and check if it builds

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+// const assetsDir = '../assets';
+
+module.exports = {
+  mode: "development",
+  entry: { "main": "./main.scss" },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: '/dist'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].css",
+            },
+          },
+          {
+            loader: "extract-loader",
+          },
+          {
+            loader: "css-loader?-url",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+
+  watch: true
+};
